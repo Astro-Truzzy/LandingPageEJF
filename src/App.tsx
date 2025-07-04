50d80804d6542daf3389b70d5e7b12086f55ee79
Williams Trust <taresy.dev@gmail.com>
2025-07-04 08:57:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import AOS from 'aos';
-import "aos/dist/aos.css";
-import HeroSection from "./Components/HeroSection";
-import Navbar from './Components/NavBar';
-import NewInvi from './Components/NewInvi';
-import Testimonials from './Components/Testimonials';
-import Footer from './Components/Footer';
-import ScrollToTop from './Components/ScrollToTop';
+// import AOS from 'aos';
+// import "aos/dist/aos.css";
+// import HeroSection from "./Components/HeroSection";
+// import Navbar from './Components/NavBar';
+// import NewInvi from './Components/NewInvi';
+// import Testimonials from './Components/Testimonials';
+// import Footer from './Components/Footer';
+// import ScrollToTop from './Components/ScrollToTop';
 
-AOS.init();
+// AOS.init();
 
-function App() {
- return (
-   <>
-   <Navbar/>
-   <HeroSection/>
-   <NewInvi/>
-   <Testimonials/>
-   <ScrollToTop/>
-  <Footer/>
-   </>
-  );
-};
+// function App() {
+//  return (
+//    <>
+//    <Navbar/>
+//    <HeroSection/>
+//    <NewInvi/>
+//    <Testimonials/>
+//    <ScrollToTop/>
+//   <Footer/>
+//    </>
+//   );
+// };
 
-export default App;
+// export default App;
